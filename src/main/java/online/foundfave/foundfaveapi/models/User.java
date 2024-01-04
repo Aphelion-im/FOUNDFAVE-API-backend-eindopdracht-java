b25bc048de75b042f3dbd3f10bbd03b0bf6a8cf8
Aphelion-im <aphelion-im@protonmail.com>
2024-01-04 19:33:41 +0100

    Implemented Search user by email, OutputDto, and Mapper: user to dto

--- src/main/java/online/foundfave/foundfaveapi/models/User.java
+++ src/main/java/online/foundfave/foundfaveapi/models/User.java
@@ -1,12 +1,18 @@
 package online.foundfave.foundfaveapi.models;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
 
-@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "users")
 public class User {
